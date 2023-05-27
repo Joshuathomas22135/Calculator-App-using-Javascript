731dcea755e34de04967342ba72ac1c110de16aa
Joshuathomas22135 <joshuatjeremiah22135@outlook.com>
2023-05-27 13:46:38 +0400

    Updated

---
+++ script.js
@@ -0,0 +1,6 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalButton = document.querySelector('[data-equal]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const perviousOperandTextElement = document.querySelector('[data-pervious-operand]') 
